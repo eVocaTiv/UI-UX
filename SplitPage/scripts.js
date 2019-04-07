01c2ccb3e0c9c75a603763c3b2580aefbc5560f3
eVocaTiV <f2015011@goa.bits-pilani.ac.in>
2019-04-08 02:38:33 +0530

    Changed styling for bg

--- SplitPage/scripts.js
+++ SplitPage/scripts.js
@@ -6,17 +6,22 @@ const container = document.querySelector('.container');
 // using arrow function
 left.addEventListener('mouseenter', () => {
     container.classList.add('hover-left');
+    container.classList.add('add-bg-left');
 }); 
 
 left.addEventListener('mouseleave', () => {
     container.classList.remove('hover-left');
+    container.classList.remove('add-bg-left');
+
 }); 
 
 right.addEventListener('mouseenter', () => {
     container.classList.add('hover-right');
+    container.classList.add('add-bg-right');
 }); 
 
 right.addEventListener('mouseleave', () => {
     container.classList.remove('hover-right');
+    container.classList.remove('add-bg-right');
 }); 
 
